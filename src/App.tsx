f3cb68079d80931a9a345274225514a02078cab5
EDWIN CHEN <edwin_c@greypanel.com>
2020-06-16 08:14:01 +0800

    Update search page result

--- src/App.tsx
+++ src/App.tsx
@@ -28,29 +28,35 @@ function App() {
   const [nextPage, setNextPage] = useState<number | undefined>()
   const [timer, setTimer] = useState<ReturnType<typeof setTimeout>>()
   const [done, setDone] = useState(false)
+  const [disabled, setDisabled] = useState(false)
 
   const ref = useRef(false)
 
   useEffect(() => {
-    loader.start()
-    axios
-      .get(
-        `http://localhost:3000/patrols?page=${currentPage}&limit=${pageSize}&search=${search}`
-      )
-      .then((response: AxiosResponse<IData>) => {
-        setResults(response.data.results)
-        if (response.data.previous) setPrevPage(response.data.previous.page)
-        else setPrevPage(undefined)
-        if (response.data.next) setNextPage(response.data.next.page)
-        else setNextPage(undefined)
+    if (search) {
+      loader.start()
+      setDisabled(true)
+      axios
+        .get(
+          `http://localhost:3000/patrols?page=${currentPage}&limit=${pageSize}&search=${search}`
+        )
+        .then((response: AxiosResponse<IData>) => {
+          setResults(response.data.results)
+          if (response.data.previous) setPrevPage(response.data.previous.page)
+          else setPrevPage(undefined)
+          if (response.data.next) setNextPage(response.data.next.page)
+          else setNextPage(undefined)
 
-        setDone(!done)
-        loader.close()
-      })
-      .catch(err => {
-        console.error('error:', err)
-        loader.close()
-      })
+          setDone(!done)
+          loader.close()
+          setDisabled(false)
+        })
+        .catch(err => {
+          console.error('error:', err)
+          loader.close()
+          setDisabled(false)
+        })
+    }
     // eslint-disable-next-line
   }, [currentPage, pageSize])
 
@@ -68,6 +74,7 @@ function App() {
 
     const t = setTimeout(() => {
       loader.start()
+      setDisabled(true)
       axios
         .get(
           `http://localhost:3000/patrols?page=1&limit=${pageSize}&search=${val}`
@@ -81,10 +88,12 @@ function App() {
 
           setDone(!done)
           loader.close()
+          setDisabled(false)
         })
         .catch(err => {
           console.error('error:', err)
           loader.close()
+          setDisabled(false)
         })
     }, 500)
 
@@ -120,77 +129,73 @@ function App() {
           value={search}
           onChange={handleSearch}
           placeholder="Type something..."
+          disabled={disabled}
         />
       </div>
-      <div className="result-header">RESULT</div>
-      <div className="results" data-testid="results">
-        {results.length > 0 &&
-          results.map((res: IResult) => {
-            if (ref.current !== done) {
-              const startIndex = res.title.toLowerCase().indexOf(search)
-              const endIndex = startIndex + search.length
-              const keyword = res.title.slice(
-                startIndex,
-                startIndex + search.length
-              )
-              const title =
-                res.title.slice(0, startIndex) +
-                '<span class="highlight">' +
-                keyword +
-                '</span>' +
-                res.title.slice(endIndex)
-              return (
-                <div className="result" key={res.id}>
-                  <strong
-                    className="title"
-                    dangerouslySetInnerHTML={{ __html: title }}></strong>
-                  <p className="description">{res.description}</p>
-                </div>
-              )
-            } else {
-              return (
-                <div className="result" key={res.id}>
-                  <strong className="title">{res.title}</strong>
-                  <p className="description">{res.description}</p>
-                </div>
-              )
-            }
-          })}
-      </div>
-      {results.length === 0 && (
+      {search && results.length > 0 && (
+        <>
+          <div className="result-header">RESULT</div>
+          <div className="results" data-testid="results">
+            {results.length > 0 &&
+              results.map((res: IResult) => {
+                const startIndex = res.title.toLowerCase().indexOf(search.toLowerCase())
+                const endIndex = startIndex + search.length
+                const keyword = res.title.slice(
+                  startIndex,
+                  startIndex + search.length
+                )
+                const title =
+                  res.title.slice(0, startIndex) +
+                  '<span class="highlight">' +
+                  keyword +
+                  '</span>' +
+                  res.title.slice(endIndex)
+                return (
+                  <div className="result" key={res.id}>
+                    <strong
+                      className="title"
+                      dangerouslySetInnerHTML={{ __html: title }}></strong>
+                    <p className="description">{res.description}</p>
+                  </div>
+                )
+              })}
+          </div>
+          <div className="pagination">
+            <div>
+              <span className="page-size">Search result number</span>
+              <select
+                className="select"
+                value={pageSize}
+                onChange={handlePageSizeChange}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
+            </div>
+            <div>
+              {prevPage && (
+                <span className="prev-page">
+                  <a href="/#" onClick={handlePreviousClick}>
+                    Previous
+                  </a>
+                </span>
+              )}
+              <span className="current-page">{currentPage}</span>
+              {nextPage && (
+                <span className="next-page">
+                  <a href="/#" onClick={handleNextClick}>
+                    Next
+                  </a>
+                </span>
+              )}
+            </div>
+          </div>
+        </>
+      )}
+      {search && results.length === 0 && ref.current !== done && (
         <div className="no-result">No results found.</div>
       )}
-      <div className="pagination">
-        <div>
-          <span className="page-size">Search result number</span>
-          <select
-            className="select"
-            value={pageSize}
-            onChange={handlePageSizeChange}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-          </select>
-        </div>
-        <div>
-          {prevPage && (
-            <span className="prev-page">
-              <a href="/#" onClick={handlePreviousClick}>
-                Previous
-              </a>
-            </span>
-          )}
-          <span className="current-page">{currentPage}</span>
-          {nextPage && (
-            <span className="next-page">
-              <a href="/#" onClick={handleNextClick}>
-                Next
-              </a>
-            </span>
-          )}
-        </div>
-      </div>
     </div>
   )
 }
